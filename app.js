5a493700b0203bb07f2b71f5a14ed2180122b70a
aracely <aracely.rivera@unmsm.edu.pe>
2024-06-07 19:10:48 -0500

    Primer Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/*
+const express = require('express')
+const app = express()
+app.get('/',function(req,res){
+    res.send('Hello World')
+})
+
+app.listen(3000)
+*/
+/*
+require('dotenv').config();
+const express = require('express')
+const app = express()
+app.get('/',function(req,res){
+    res.send('Hello World')
+})
+app.listen(process.env.PORT,()=>{
+    console.log('Servidor corriendo en puerto',process.env.PORT)
+})
+
+*/
+require('dotenv').config();
+const Server = require('./models/server');
+const server= new Server();
+server.listen();
+
---
+++ models/server.js
@@ -0,0 +1,61 @@
+/*
+const express=require('express')
+
+class Server{
+    constructor(){
+        this.app=express();
+        this.port=process.env.PORT;
+        //Middelware
+        this.middlewares();
+        //Rutas de la aplicacion
+        this.routes();
+    }
+
+    routes(){
+        this.app.get('/api',(req,res)=>{
+              //res.json('Hello World');
+              res.send('Hello World');
+        })
+    }
+    listen(){
+        this.app.listen(this.port,()=>{
+            console.log('Servidor corriendo en el puerto',this.port)
+        })
+    }
+
+    middlewares(){
+        this.app.use(express.static('public'));
+    }
+}
+module.exports = Server;
+*/
+const express=require('express')
+const cors = require('cors');
+class Server{
+    constructor(){
+        this.app=express();
+        this.port=process.env.PORT;
+        this.usuariosPath='/api/usuarios'
+        //Middelware
+        this.middlewares();
+        //Rutas de la aplicacion
+        this.routes();
+    }
+
+    routes(){
+        this.app.use(this.usuariosPath,require('../routes/usuarios'))
+    }
+    listen(){
+        this.app.listen(this.port,()=>{
+            console.log('Servidor corriendo en el puerto',this.port)
+        })
+    }
+
+    middlewares(){
+        this.app.use(cors());
+        //lectura y parseo del body recibe lo que se envia
+        this.app.use(express.json());
+;        this.app.use(express.static('public'));
+    }
+}
+module.exports = Server;

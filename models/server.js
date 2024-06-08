fd242fe45a9acb8ffae03e5fa6d4017d4177e4a8
aracelyRiv <138259493+aracelyRiv@users.noreply.github.com>
2024-06-07 19:41:32 -0500

    Update server.js

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const express=require('express')
 class Server{
     constructor(){
         this.app=express();
-        this.port=process.env.PORT;
+        this.port=process.env.PORT || 3000;
         //Middelware
         this.middlewares();
         //Rutas de la aplicacion
